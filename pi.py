db38597be6850553057edff0a84c0473ed606545
Fabrizio Contigiani <fabcontigiani@gmail.com>
2022-03-31 23:44:52 -0300

    Spacing

--- pi.py
+++ pi.py
@@ -5,7 +5,7 @@ counter = 0
 for i in range(iterations):
     x = random.random()
     y = random.random()
-    if (x**2 + y**2)**(1/2)<=1:
+    if (x**2 + y**2)**(1/2) <= 1:
         counter += 1
 
 print((counter/iterations)*4)
